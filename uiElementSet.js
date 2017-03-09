733b51bc8042debb228c67c210e800a519ba6e4f
fer-sd <fernando.serranodoncel@telefonica.com>
2017-03-09 14:46:11 +0100

    V.1.2 uiElement() - V.13 uiElementSet()
    Añadidos métodos de acciones.

--- uiElementSet.js
+++ uiElementSet.js
@@ -3,7 +3,7 @@ Clase UIElementSet - Integrador
 */
 
 //Versión uiElementSet.js
-var uiElementSetVersion = 1.1;
+var uiElementSetVersion = 1.3;
 
 function uiElementSet(){
 
@@ -11,14 +11,78 @@ function uiElementSet(){
 
 	/***** Variables privadas ******/
         
-    // Array con los uiElements instanciados
+    //Array con los uiElements instanciados
     var uiElements = [];
 
-    // Array con las posibles acciones
-    var actions = [];    
+	var actionSet = [];
+
+	actionSet[0]="console.log('Encuesta creada - Version encuesta CSI: '+csi.getVersion());";
+	actionSet[1]="console.log('Modal encuesta lanzado - Version encuesta CSI: '+csi.getVersion());$('.usabilla_live_button_container').css('display','none');console.log('Moquillo encuesta Usabilla cerrado por Integrador');setTimeout(function(){csi.cerrarModalEncuesta();console.log('Modal encuesta cerrado por Integrador');}, 3000);";
+	actionSet[2]="console.log('Modal encuesta cerrado - Version encuesta CSI: '+csi.getVersion());";
+	actionSet[3]="console.log('Modal inicial lanzado - Version encuesta CSI: '+csi.getVersion());setTimeout(function(){csi.cerrarModalInicial();console.log('Modal inicial cerrado por Integrador');}, 3000);";
+	actionSet[4]="console.log('Modal inicial cerrado - Version encuesta CSI: '+csi.getVersion());";
+	actionSet[5]="console.log('Modal minimizado lanzado - Version encuesta CSI: '+csi.getVersion());";
+	actionSet[6]="console.log('Modal minimizado cerrado - Version encuesta CSI: '+csi.getVersion());";
 
 
 	/***** Métodos privados ******/
+
+	/**
+	* Generador de la función que se pasará como parámetro al uiElement. Debe agregar el switch
+	* @param 
+ 	* @return:
+	*/
+	var actionsFunctionGenerator = function(){
+	}
+
+	/**
+	* Inserta acciones seleccionadas en el uiElement
+	* @param 
+ 	* @return:
+	*/
+
+	var actionsArrayGenerator = function(selectedActions){
+		var myActionsArray = [];
+			for (c=0 ; c<selectedActions.length; c++)
+				myActionsArray.push(actionSet[c]);
+	    return myActionsArray;
+	}
+
+
+	/**
+	* Método que asocia un id de uiElement con un set de funciones a ejecutar en cada estado
+	* @param id: id del uiElement que se desea obtener (proveedor-servicio)
+ 	* @return: functionsArray. Array con las funciones a ejecutar por el uiElement
+	*/
+	var actionsSwitcher = function(id){
+		try{
+			switch (id){
+				case "csi-encuesta":
+					//Llamaos a generador de array de acciones con las posiciones de las acciones seleccionadas
+					return actionsArrayGenerator([0,1,2,3,4,5,6]);	
+					break;
+				/**
+				case "inbenta-chat":
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					break;
+				case "tealium-login":
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					functionsArray.push(actions[]);
+					break;
+				case:
+					break;
+				**/
+				default:
+					return false;
+			}
+		}
+		catch (e){console.log(e)}
+	}
 
 
 	/***** PARTE PÚBLICA ******/
@@ -28,7 +92,7 @@ function uiElementSet(){
 	/***** Métodos públicos ******/
 
 	/**
-	* Método para insertar un uiElement (objeto) pasado como parámetro en el uiElementSet
+	* Constructor de uiElements en base al objeto pasado como parámetro
     * @param object: String con el objeto asociado al uiElement instanciado
  	* @return: si se realiza correctamente la operación, devuelve true, en caso contrario, false
 	*/
@@ -37,10 +101,15 @@ function uiElementSet(){
 			var myObject = window[object];
 
 	    	if ((typeof(myObject)!=null) || (typeof(myObject)!=undefined)){
+	    		/* Inicio constructor del uiElement */
 		    	//Instanciación del uiElement
 		    	var myUiElement = new uiElement(myObject);
 		    	//Asignación de nombre de objeto original
 		    	myUiElement.setObjectName(object);
+		    	//Añadir reglas
+		    	myUiElement.setActions(actionsSwitcher(myUiElement.getId()));
+		    	//Asignar acciones
+
 				 //Añadir elemento al set
 				uiElements.push(myUiElement);
 				return true;
@@ -63,6 +132,7 @@ function uiElementSet(){
 	            	return uiElements[c];
 	        }
 			return false;
+		}
 		catch (e){console.log(e)}	
 	}
 
@@ -119,33 +189,6 @@ function uiElementSet(){
 	*/
 	this.getUiElementSetSize = function(){
 		return uiElements.length;
-	}
-
-	/**
-	* Añade una acción
-	* @param: 
- 	* @return: 
-	*/
-	this.addAction = function(action){
-		actions.push(action);
-	}
-
-	/**
-	* Devuelve una acción
-	* @param 
- 	* @return: 
-	*/
-	this.getAction = function(){
-		//return actions[];
-	}
-
-	/**
-	* Elimina una acción
-	* @param id: id del uiElement que se desea eliminar
- 	* @return: número de uiElements que contiene actualmente el uiElementSet
-	*/
-	this.removeAction = function(action){
-		//remove action;
 	}
 }
 
